0e1c99809e8ae036cde8c263ab3dd5e3766972f8
Aruche-code <mo.crew2435@gmail.com>
2025-01-25 20:34:55 +0900

    コミット打消し

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,51 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-
-      // セミコロンを強制、fixコマンドで強制変換可能。
-      semi: ['error', 'always'],
-
-      // シングルクオーテーションを強制、fixコマンドで強制変換可能。
-      quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
-
-      // 混合演算子に対して括弧を強制、強制変換不可。
-      'no-mixed-operators': [
-        'error',
-        {
-          'groups': [
-            ['+', '-', '*', '/', '%', '**'],
-            ['&', '|', '^', '~', '<<', '>>', '>>>'],
-            ['==', '!=', '===', '!==', '>', '>=', '<', '<='],
-            ['&&', '||'],
-          ],
-          'allowSamePrecedence': false, // 演算子の同じ優先順位を許可するかどうか。
-        }
-      ],
-
-      // TypeScriptのメンバーデリミタをカンマに限定
-      '@typescript-eslint/member-delimiter-style': ['error', {
-        multiline: {
-          delimiter: 'comma',  // 複数行のメンバーデリミタをカンマに設定
-          requireLast: false
-        },
-      }],
-
-      // 型定義に<>を必ず使用するためのルール設定例
-      '@typescript-eslint/consistent-type-assertions': [
-        'error',
-        {
-          assertionStyle: 'angle-bracket',
-          objectLiteralTypeAssertions: 'never'
-        }
-      ]
-    }
-  }
 ];
 
 export default eslintConfig;
